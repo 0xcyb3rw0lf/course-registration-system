179fd984d779ce44fb4e200e9793514046300eba
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-04-10 16:05:26 +0300

    Added UOB Logo with University of Bahrain to the Header Closes #1

--- header.php
+++ header.php
@@ -1,39 +1,19 @@
-<?php
-if (isset($_SESSION["activeUser"])) { // if the user is signed in
-?>
-    <div id="container">
-        <header>
-            <a href="home.php" id="logo">
-                <p>UOB Course Registration System</p>
-            </a>
-            <div id="nav">
-                <!-- TODO: add necessary icons for navigating the system -->
-                <a href="home.php">Home Page</a>
-                <a href="catalogue.php">Catalogue</a>
-                <a href="history.php">My History</a>
-                <a href="myprofile.php">My Profile</a>
-                <a href="signout.php" class="butn">Sign out</a>
-            </div>
-        </header>
-    </div>
-<?php
-} else {
-    // if the user is not logged in
-?>
-    <div id="container">
-        <header>
-            <a href="home.php" id="logo">
-                <p>BBay</p>
-            </a>
-            <div id="nav">
-                <!-- TODO: add necessary icons for navigating the system -->
-                <a href="home.php">Home</a>
-                <a href="catalogue.php">Catalogue</a>
-                <a href="signin.php" class="butn">Sign in</a>
-                <a href="signup.php" class="butn primary-butn">Create Account</a>
-            </div>
-        </header>
-    </div>
-<?php
-}
-?>
+<!-- if (isset($_SESSION["activeUser"])) { // if the user is signed in -->
+
+<link rel="stylesheet" href="css/style.css">
+<div id="container">
+    <header>
+        <a href="home.php" id="logo">
+            <img class="uob-logo" src="/course-registration-system/images/uob-logo.png" alt="University of Bahrian Logo">
+            <p style="margin-left: 1.5em;">UOB Course Registration System</p>
+        </a>
+        <div id="nav">
+            <!-- TODO: add necessary icons for navigating the system -->
+            <a href="home.php">Home Page</a>
+            <a href="catalogue.php">Catalogue</a>
+            <a href="history.php">My History</a>
+            <a href="myprofile.php">My Profile</a>
+            <a href="signout.php" class="butn">Sign out</a>
+        </div>
+    </header>
+</div>
