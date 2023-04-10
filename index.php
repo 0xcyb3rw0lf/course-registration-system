7858798222faa5f48e2149f282eb86be50a4096a
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-04-10 14:04:33 +0300

    Removed Bootstrap content from index.php

--- index.php
+++ index.php
@@ -15,12 +15,6 @@
 
     <!-- Adding the css files -->
     <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/bootstrap-responsive.css">
-    <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
     <link rel="stylesheet" href="css/style.css" />
 
     <!-- Adding the Fonts -->
