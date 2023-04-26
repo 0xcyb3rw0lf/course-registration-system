484cc10a8ff5ee2c89b0e807b72d1c980b3bd77c
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-04-26 17:42:57 +0300

    Edited the list of professor services.php

--- services.php
+++ services.php
@@ -3,9 +3,9 @@ $professor = array(
     array(
         "Academic Services:" => array(
             "Manage Grades" => "professor/professor-grades.php",
+            "Manage Appealing Requests" => "professor/professor-grades.php",
             "Manage Attendance" => 'professor/professor-view-section.php',
             "View Section" => 'professor/professor-view-section.php',
-
         )
     )
 );
