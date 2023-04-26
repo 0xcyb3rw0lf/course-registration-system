40fd62ad2470e7ab11fe3763cc27090ccf5283bd
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-04-26 16:17:46 +0300

    Added checkInput() to functions.php
    checkInput() does necessary input filteration
    and santization to prevent attacks

--- functions.php
+++ functions.php
@@ -8,3 +8,20 @@
  * 
  * @author Omar Eldanasoury
  */
+
+ 
+/**
+ * @name checkInput
+ * Performs input sanitization on user input
+ * to prevent injection attacks. It encodes
+ * special characters, removes slaches,
+ * and removes extra spaces. 
+ * 
+ * @author Omar Eldanasoury
+ * @param $input user Input as text
+ * @return string sanitized secure text to process.
+ */
+function checkInput($input)
+{
+    return htmlspecialchars(stripslashes(trim($input)));
+}
