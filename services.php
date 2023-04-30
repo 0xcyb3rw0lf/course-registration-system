41e6443d83dc34790522c00ba3f92ad946994402
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-04-30 17:08:17 +0300

    Added functions to services.php to retrieve user's services, #11
    Still needs work on the list of services for all the users

--- services.php
+++ services.php
@@ -1,9 +1,121 @@
 <?php
-$professor = array(
+require_once("functions.php");
 
-    "Manage Grades" => "professor/professor-grades.php",
-    "Manage Appealing Requests" => "professor/professor-grades.php",
-    "Manage Attendance" => 'professor/professor-view-section.php',
-    "View Section" => 'professor/professor-view-section.php',
+/**
+ * Returns the list of services
+ * that should be shown to professors.
+ * 
+ * @author Omar Eldanasoury
+ * @return array<string> Professor's list of services
+ */
+function getProfessorList()
+{
+    return array(
+        "Manage Grades" => "professor/professor-grades.php",
+        "Manage Appealing Requests" => "professor/professor-grades.php",
+        "Manage Attendance" => 'professor/professor-view-section.php',
+        "View Section" => 'professor/professor-view-section.php',
+    );
+}
 
-);
+/**
+ * Returns the list of services
+ * that should be shown to students.
+ * 
+ * @author Omar Eldanasoury
+ * @return array<string> Student's list of services
+ */
+function getStudentList()
+{
+    return array(
+        "Course Registration" => "student/",
+        "Course Schedule" => "student/",
+        "View Attendance" => 'student/',
+        "View Transcript" => 'student/',
+        "Simulate GPA" => 'student/',
+        "Request Summer Seat" => 'student/',
+    );
+}
+
+/**
+ * Returns the list of services
+ * that should be shown to admins.
+ * 
+ * @author Omar Eldanasoury
+ * @return array<string> Admin's list of services
+ */
+function getAdminList()
+{
+    return array(
+
+        "Manage Grades" => "professor/professor-grades.php",
+        "Manage Appealing Requests" => "professor/professor-grades.php",
+        "Manage Attendance" => 'professor/professor-view-section.php',
+        "View Section" => 'professor/professor-view-section.php',
+
+    );
+}
+
+/**
+ * Returns the list of services
+ * that should be shown to head
+ * of departmentusers.
+ * 
+ * @author Omar Eldanasoury
+ * @return array<string> HOD's list of services
+ */
+function getHodList()
+{
+    return array(
+
+        "Manage Grades" => "professor/professor-grades.php",
+        "Manage Appealing Requests" => "professor/professor-grades.php",
+        "Manage Attendance" => 'professor/professor-view-section.php',
+        "View Section" => 'professor/professor-view-section.php',
+
+    );
+}
+
+/**
+ * Returns the list of services
+ * that should be shown to dean
+ * users.
+ * 
+ * @author Omar Eldanasoury
+ * @return array<string> Dean's list of services
+ * 
+ */
+function getDeanList()
+{
+    return array(
+        // TODO: Fill the paths of these pages
+        "View Seat Requests" => "dean-hod/",
+        "View Students" => "dean-hod/",
+        "View Staff" => 'dean-hod/',
+        "Edit Course Details" => 'dean-hod/',
+        "Generate Reports" => 'dean-hod/'
+
+    );
+}
+
+/**
+ * returns the list of servies
+ * based on the type of the current
+ * logged in user
+ * 
+ * @author Omar Eldanasoury
+ * @return array<string> User's List as an array of string
+ */
+function getUserList($userType)
+{
+    if ($userType == 'student')
+        return getStudentList();
+    else if ($userType == 'admin')
+        return getAdminList();
+    else if (($userType == 'head of department'))
+        return getHodList();
+    else if ($userType == 'professor')
+        return getProfessorList();
+    else
+        return getDeanList();
+}
