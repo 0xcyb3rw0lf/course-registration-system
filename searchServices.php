95f22a2264548a2a7b63e822dc15296e77df9eea
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-04-30 17:55:16 +0300

    Done searchServices.php, #14

--- searchServices.php
+++ searchServices.php
@@ -1,1 +1,25 @@
 <?php
+
+/**
+ * searchServices.php
+ * 
+ * gets what the user wrote in
+ * the search box and searchs
+ * the list of services for similar
+ * titles.
+ * 
+ * @return string of found services.
+ * @author Omar Eldanasoury
+ */
+
+require_once("services.php"); // already has included funcitons.php inside it
+if (isset($_GET["text"]))
+    $userText = checkInput($_GET["text"]); // called checkInput() to sanitize the input from user
+
+$output = '';
+$services = getUserServicesList(getUserTypeAsText($_SESSION["activeUser"][1]));
+foreach ($services as $service)
+    if (str_contains($service, $userText))
+        $output += "#$service";
+
+echo $output;
