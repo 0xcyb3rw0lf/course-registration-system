98379a197249d11dc477b65037d80e11b5f5aa0a
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-05-13 14:47:32 +0300

    Completed admin/update-section.php with input validation, #22

--- admin/update-section.php
+++ admin/update-section.php
@@ -14,8 +14,7 @@ if (!isset($_SESSION["activeUser"])) // if the user is not logged in he will be 
     header("location: /course-registration-system/login.php");
 
 require_once("../functions.php");
-if (isset($_POST["manage-appealing-requests"])) {
-
+if (isset($_POST["view-section"])) {
     // TODO: after user confirms (using JS)
     // first get data
     $cid = checkInput($_POST["course-code"]);
@@ -24,56 +23,50 @@ if (isset($_POST["manage-appealing-requests"])) {
 
     if ($cid == "" or $secId == "") { // if the user didn't choose a value for the course or the section
         $feedbackMsg = "<span class='failed-feedback'>Please select a course and a section!</span>";
-    } else {
-        $students = getAppealingRequests($secId);
-        $tableBody = "";
-        unset($submitGrades);
-        $submitGrades = true;
-
-        // generating the table body based on the data
-        $eachStudent = preg_split("/#/", $students);
-        if (empty($eachStudent[0])) {
-            $tableBody = "<tr><td colspan='3'>You Have No Active Appealing Requests!</td></tr>";
-            $submitGrades = false;
-        } else {
-            foreach ($eachStudent as $studentData) {
-                $piecesOfData = preg_split("/@/", $studentData);
-                // add the complete table row for each student to the table body
-                if ($piecesOfData[0] == "")
-                    continue; // solves the null issue, where it prints empty values
-                // $piecesOfData[0] = id
-                // $piecesOfData[1] = name
-                // $piecesOfData[2] = grade
-                $tableBody .= "\n<tr>\n<td>" . $piecesOfData[0] . "</td>\n<td>"
-                    . $piecesOfData[1] . "</td>\n<td>"
-                    . '<input style="font-size: medium; padding: 0.1em; width: min-content; border-radius: 0.1em" type="number" min="0" max="100" step=".01" name="grade[]" value="' . $piecesOfData[2] . '" />'
-                    //below, we store the id of the student
-                    . "\n<input type='hidden' name='student-id[]' value='" . $piecesOfData[0] . "'/>\n</td>\n</tr>";
-            } // after this, the table will shown as html
-        }
+    } else { // display the form to enter new section details
+        // shows the form for the user to enter new information for the section
+        $displayForm = true;
+        $sectionData = getSectionData($_SESSION["section"]); // passing the id of the section user selected
     }
 }
 
 // Now submitting the grade into the db
-if (isset($_POST["update-grades"])) {
+if (isset($_POST["update-section"])) {
     $sectionId = $_SESSION["section"];
-    $grades = $_POST["grade"]; // now we have the list of grades
-    $ids = $_POST["student-id"];
+    $courseId = $_POST["course-code"];
+    $professorId = $_POST["prof-name"];
+    $sectionNum = checkInput($_POST["section-num"]);
+    $buildingId = $_POST["bldng"];
+    $roomId = $_POST["room"];
+    $dateTime = $_POST["datetime"];
+    $days = $_POST["days"];
+    $semId = getCurrentSemesterId();
 
-    $hasEmptyGrade = false; // check if it has a place were there is no grade
-    for ($i = 0; $i < count($grades); $i++)
-        if ($grades[$i] == "") {
-            $hasEmptyGrade = true;
-            break;
-        }
-
-    if ($hasEmptyGrade) {
-        $feedbackMsg = "<span class='failed-feedback'>Please enter grades for all students!</span>";
+    // check for empty section number, or any empty value
+    if (
+        empty($sectionId)
+        or empty($courseId)
+        or empty($professorId)
+        or (empty($sectionNum))
+        or empty($buildingId)
+        or empty($roomId)
+        or empty($dateTime)
+        or empty($days)
+    ) {
+        $feedbackMsg = "<span class='failed-feedback'>Please enter all fields as required!</span>";
+    } else if (!preg_match("/\d+/", $sectionNum)) { // if the user entered wrong value for section number\
+        $feedbackMsg = "<span class='failed-feedback'>Enter only numbers for section number!</span>";
     } else {
-        if (closeAppealingRequest($sectionId, $ids, $grades)) {
-            $feedbackMsg = "<span class='success-feedback'>Grades Updated Successfully!</span>";
-        } else {
-            $feedbackMsg = "<span class='failed-feedback'>Error Updating Grades!</span>";
+        try {
+            if (updateSection($_SESSION["section"], $courseId, $professorId, $dateTime, $sectionNum, $roomId, $days)) {
+                $feedbackMsg = "<span class='success-feedback'>Section is Updated Successfully!</span>";
+            } else { // if updateSection() returned false
+                $feedbackMsg = "<span class='failed-feedback'>Error Updating Section!</span>";
+            }
+        } catch (LogicException $samePreviousTimeRoomDays) { // if there is a time conflict, an exception will be thrown by updateSection()
+            $feedbackMsg = "<span class='failed-feedback'>You Chose The Same Time, Room, And Days!<br>Please Choose Different Time/Room/Day Other Than Existing One(s)!</span>";
+        } catch (Exception $exception) { // if there is a time conflict, an exception will be thrown by updateSection()
+            $feedbackMsg = "<span class='failed-feedback'>Time Conflict Exists, Please Choose Another Time/Days!</span>";
         }
     }
 }
@@ -111,7 +104,6 @@ if (isset($_POST["update-grades"])) {
 <body>
 
     <?php require("../header.php");
-    require_once("../functions.php");
     // TODO: do these 2 functions and getProfessorSection() and display data, no validation required here,
     // also show if the section is empty
 
@@ -155,7 +147,7 @@ if (isset($_POST["update-grades"])) {
                 </div>
             </div>
 
-            <input type="submit" class="butn primary-butn sign-butn no-margin-left margin-top small" name="manage-appealing-requests" id="manage-appealing-requests" value="View Apealing Requests">
+            <input type="submit" class="butn primary-butn sign-butn no-margin-left margin-top small" name="view-section" id="view-section" value="View Section To Update">
             <br><br>
             <?php
             if (isset($feedbackMsg)) {
@@ -165,30 +157,91 @@ if (isset($_POST["update-grades"])) {
             ?>
         </form>
         <!-- The Table of students list -->
-        <div class="catalogue-main" style="margin-bottom: 2em;">
+        <div class="catalogue-main" style="margin-bottom: 2em; text-align: left;">
             <form method="post" class="form" style="margin-left: 2.75em;  text-align: center;">
-                <table id="displayTable" <?php if (isset($tableBody)) echo "style='visibility: visible; margin: 0;'";
-                                            else echo "style='visibility: hidden;'" ?>>
-                    <thead>
-                        <tr>
-                            <th class="th-color">Student ID</th>
-                            <th class="th-color">Student Name</th>
-                            <th class="th-color">Grade</th>
+                <?php
+                if (isset($displayForm) and $displayForm) {
+                    // $sectionInfo = getSectionInformation(); // an array
+                    // Required varialbes for adding the section
+                    $courses = getCourses(); // get the courses list from the database
+                    $professorNames = getProfessorNames();
+                    $buildings = getBuildings();
+                    $rooms = getRooms();
+                ?>
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <!--  Here we add the dynamic content from the database -->
-                        <?php
-                        if (isset($tableBody))
-                            echo $tableBody;
-                        ?>
-                    </tbody>
-                </table>
-                <br><br><br>
-                <input <?php if (isset($tableBody) and !(isset($submitGrades) and !$submitGrades)) echo "style='visibility: visible; width: 35%;'";
-                        else echo "style='visibility: hidden;'" ?> <?php if (isset($submitGrades) and !$submitGrades) echo "style='visibility: hidden;";
-                                                                    unset($submitGrades); ?> onclick="return confirm('Do you want to update the grades for this section?')" type="submit" class="butn primary-butn sign-butn no-margin-left margin-top small" name="update-grades" id="update-grades" value="Update Student's Grades">
+                    <div class="attendance-flex catalogue-main">
+                        <!-- Course Code and Section Number -->
+                        <div class="attendance-inner-flex">
+                            <label for="course-code">Course Code:</label><br><br>
+                            <select class="selecter" name="course-code" id="course-code">
+                                <?php
+                                if ($courses != array())
+                                    for ($i = 0; $i < count($courses); $i++)
+                                        foreach ($courses[$i] as $id => $code) {
+                                            echo "<option value='" . strval($id) . "'>" . $code . "</option>";
+                                        }
+
+                                ?>
+                            </select>
+                            <br><br>
+                            <!-- Section Number -->
+                            <label for="section-num">Section Number:</label><br><br>
+                            <input type="number" min="1" class="selecter" name="section-num" id="section-num">
+                        </div>
+
+                        <!-- Building and Room -->
+                        <div class="attendance-inner-flex" style="margin-left: 2.5em;">
+                            <label for="bldng">Building:</label><br><br>
+                            <select onchange="getRooms(this.value)" class="selecter" name="bldng" id="bldng">
+                                <option value="">Select a Building</option>
+                                <?php
+                                if ($buildings != array())
+                                    for ($i = 0; $i < count($buildings); $i++)
+                                        foreach ($buildings[$i] as $id => $name) {
+                                            echo "<option value='" . strval($id) . "'>" . $name . "</option>";
+                                        }
+
+                                ?>
+                            </select>
+                            <br><br><br>
+                            <label for="room">Room:</label><br><br>
+                            <select class="selecter" name="room" id="room">
+                                <option value="">Select a Room</option>
+                                <!-- The options will be optained from the database using AJAX and PHP -->
+                                <!-- Refer to the script at the end of the page, after <body> -->
+                            </select>
+                        </div>
+
+                        <!-- Professor and Date+Time -->
+                        <div class="attendance-inner-flex" style="margin-left: 2.5em;">
+                            <label for="prof-name">Professor:</label><br><br>
+                            <select class="selecter" name="prof-name" id="prof-name">
+                                <?php
+                                if ($professorNames != array())
+                                    for ($i = 0; $i < count($professorNames); $i++)
+                                        foreach ($professorNames[$i] as $id => $name) {
+                                            echo "<option value='" . strval($id) . "'>" . $name . "</option>";
+                                        }
+
+                                ?>
+                            </select>
+                            <br><br><br>
+                            <label for="datetime">Days:</label><br><br>
+                            <select class="selecter" name="days" id="days">
+                                <option value="UTH">UTH</option>
+                                <option value="MW">MW</option>
+                            </select>
+                        </div>
+
+                        <div class="attendance-inner-flex" style="margin-left: 2.5em;">
+                            <label for="datetime">Time:</label><br><br>
+                            <input type="time" name="datetime" id="datetime">
+                        </div>
+                    </div>
+                    <input onclick="return confirm('Do you want to update the information for this section?')" type="submit" class="butn primary-butn sign-butn no-margin-left margin-top small" name="update-section" id="update-section" value="Update Section">
+                <?php
+                }
+                ?>
             </form>
         </div>
     </main>
@@ -242,6 +295,58 @@ if (isset($_POST["update-grades"])) {
     function clearSectionNumber() {
         document.getElementById("section-number").innerHTML = "<option value=''>Select a Section</option>";
 
+    }
+</script>
+
+<!-- Script for getting the rooms after the user selects the buildings :: Using AJAX
+     Author: Omar Eldanasoury
+ -->
+<script>
+    /**@@function getRooms()
+     * sends user choice to the script to get
+     * rooms of the building
+     * 
+     * @author Omar Eldanasoury
+     */
+    function getRooms(buildingId) {
+        if (buildingId.length == 0) {
+            clearRooms();
+
+            return;
+        }
+
+        const request2 = new XMLHttpRequest();
+        request2.onload = showRooms;
+        request2.open("GET", "getRooms.php?id=" + buildingId);
+        request2.send();
+    }
+
+    /**@function showRooms
+     * populated the options inside <select>
+     * after getting the rooms
+     * 
+     * @author Omar Eldanasoury
+     */
+    function showRooms() {
+        clearRooms();
+        results = this.responseText.split("#");
+        for (let result of results) {
+            idAndRoom = result.split("@");
+            if (idAndRoom[0] == '')
+                continue;
+            document.getElementById("room").innerHTML += "\n<option value='" + idAndRoom[0] + "'>" + idAndRoom[1] + "</option>";
+        }
+    }
+
+    /**@function clearRooms
+     * Clears the options to
+     * populate new options when
+     * user's choice changes.
+     * 
+     * @author Omar Eldanasoury
+     */
+    function clearRooms() {
+        document.getElementById("room").innerHTML = "<option value=''>Select a Room</option>"
     }
 </script>
 
