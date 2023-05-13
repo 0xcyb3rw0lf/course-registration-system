dee7d52fb517af93e8f2a4007b6400fd0480da11
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-05-13 19:49:00 +0300

    Added header links for logged in users

--- header.php
+++ header.php
@@ -1,19 +1,29 @@
-<!-- if (isset($_SESSION["activeUser"])) { // if the user is signed in -->
+<?php
+if (isset($_SESSION["activeUser"])) { // if the user is signed in
+?>
+    <link rel="stylesheet" href="css/style.css">
+    <div id="container">
+        <header>
+            <a href="home.php" id="logo">
+                <img class="uob-logo" src="/course-registration-system/images/uob-logo.png" alt="University of Bahrian Logo">
+                <p style="margin-left: 1.5em;">UOB Course Registration System</p>
+            </a>
+            <div id="nav">
+                <a href="/course-registration-system/index.php">Home Page</a>
+                <a href="#">
+                    Services
+                </a>
+                <a href="/course-registration-system/help.php">Help</a>
+                <a href="/course-registration-system/logout.php" class="butn">Log out</a>
+            </div>
+        </header>
+    </div>
 
-<link rel="stylesheet" href="css/style.css">
-<div id="container">
-    <header>
-        <a href="home.php" id="logo">
-            <img class="uob-logo" src="/course-registration-system/images/uob-logo.png" alt="University of Bahrian Logo">
-            <p style="margin-left: 1.5em;">UOB Course Registration System</p>
-        </a>
-        <div id="nav">
-            <!-- TODO: add necessary icons for navigating the system -->
-            <a href="home.php">Home Page</a>
-            <a href="catalogue.php">Catalogue</a>
-            <a href="history.php">My History</a>
-            <a href="myprofile.php">My Profile</a>
-            <a href="/course-registration-system/logout.php" class="butn">Sign out</a>
-        </div>
-    </header>
-</div>
+
+<?php
+} else {
+?>
+
+<?php
+}
+?>
