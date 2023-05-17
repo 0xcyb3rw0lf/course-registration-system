7d6579af07c62aa0b6b0dc44cdd7804848a71fcc
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-05-17 16:43:34 +0300

    Fixed page title in help.php

--- help.php
+++ help.php
@@ -18,7 +18,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Appealing Requests</title>
+    <title>Get Help!</title>
 
     <!-- Adding the css files -->
     <link rel="stylesheet" href="../css/reset.css">
