1e1bbb5195519af3be3625ec6d64013245070bd8
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-05-17 15:29:20 +0300

    Initial commit of student/appealing-request.php

---
+++ student/appealing-request.php
@@ -0,0 +1,166 @@
+<?php
+
+/**
+ * Delete Section Page
+ * Allows the admin user to 
+ * delete sections from the system
+ * 
+ * @author Omar Eldanasoury 
+ */
+
+session_start();
+if (!isset($_SESSION["activeUser"])) // if the user is not logged in he will be redirected to the sign up page
+    header("location: /course-registration-system/login.php");
+
+if (isset($_POST["delete-section"])) {
+    require_once("../functions.php");
+    // first get data
+    $cid = checkInput($_POST["course-code"]);
+    $secId = checkInput($_POST["section-number"]);
+
+    // then validate user input
+    if ($cid == "" or $secId == "") { // if the user didn't choose a value for the course or the section
+        $feedbackMsg = "<span class='failed-feedback'>Please select a course and a section!</span>";
+    } else {
+        // then delete the section
+        if (deleteSection($secId)) {
+            $feedbackMsg = "<span class='success-feedback'>Section is deleted successfully!</span>";
+        } else {
+            $feedbackMsg = "<span class='failed-feedback'>Error deleting seciton<br>Please try again later!</span>";
+        }
+    }
+}
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delete Section</title>
+
+    <!-- Adding the css files -->
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/style.css" />
+
+    <!-- Adding the Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+    </style>
+
+    <!-- Adding FontAwesome Kit -->
+    <script src="https://kit.fontawesome.com/163915b421.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <?php require("../header.php");
+    require_once("../functions.php");
+    // Required varialbes for adding the section
+    $courses = getCourses(); // get the courses list from the database
+    ?>
+
+
+    <main class="payment-main" style="background-color: white; background-image: none; text-align: left;">
+        <h1 class="catalogue-header" style="color: #4056A1;">Delete Section</h1>
+        <form method="post" class="form" style="margin-left: 2.75em;">
+            <div class="attendance-flex catalogue-main">
+                <!-- Course Code and Section Number -->
+                <div class="attendance-inner-flex">
+                    <label for="course-code">Course Code:</label><br><br>
+                    <select class="selecter" onchange="getSections(this.value)" name="course-code" id="course-code">
+                        <option value="">Select a Course</option>
+                        <?php
+                        if ($courses != array())
+                            for ($i = 0; $i < count($courses); $i++)
+                                foreach ($courses[$i] as $id => $code) {
+                                    echo "<option value='" . strval($id) . "'>" . $code . "</option>";
+                                }
+                        ?>
+                    </select>
+                </div>
+
+                <div class="attendance-inner-flex">
+                    <!-- Section Number -->
+                    <label for="section-number">Section Number:</label><br><br>
+                    <!-- Will be populated automatically by the system after selecting the course code, again by AJAX -->
+                    <select class="selecter" name="section-number" id="section-number" style="margin-left: 0">
+                        <option value="">Select a Course First</option>
+                        <!-- Will be filled by AJAX -->
+                    </select>
+                </div>
+            </div>
+
+            <input type="submit" class="butn primary-butn sign-butn no-margin-left margin-top small" onclick="return confirm('Are you sure you want to delete this section?')" name="delete-section" id="delete-section" value="Delete Section">
+            <?php
+            if (isset($feedbackMsg)) {
+                echo $feedbackMsg;
+                unset($feedbackMsg);
+            }
+            ?>
+        </form>
+
+    </main>
+
+    <?php require("../footer.php") ?>
+</body>
+<!-- Script for getting the sections after the user selects the course code :: Using AJAX
+     Author: Omar Eldanasoury
+ -->
+<script>
+    /**@function getSection
+     * gets the successive section number to be added
+     * for a particular course id; using AJAX.
+     * 
+     * @author Omar Eldanasoury
+     */
+    function getSections(courseId) {
+        if (courseId == "") {
+            return;
+        }
+
+        const request = new XMLHttpRequest();
+        request.onload = showSections;
+        request.open("GET", "getSections.php?cid=" + courseId);
+        request.send();
+    }
+
+    /**@function showSection
+     * Shows the section number
+     * retrieved from the database
+     * to the user through html.
+     */
+    function showSections() {
+        clearSectionNumber();
+        if (this.responseText.length == 0) {
+            document.getElementById("section-number").innerHTML += "\n<option value=''>No Sections Available</option>";
+            return
+        }
+        results = this.responseText.split("#");
+        for (let result of results) {
+            idAndNum = result.split("@");
+            if (idAndNum[0] == '')
+                continue;
+            document.getElementById("section-number").innerHTML += "\n<option value='" + idAndNum[0] + "'>" + idAndNum[1] + "</option>";
+        }
+    }
+
+    /**@function clearSectionNumber
+     * clears the html that shows the section number
+     */
+    function clearSectionNumber() {
+        document.getElementById("section-number").innerHTML = "<option value=''>Select a Section</option>";
+
+    }
+</script>
+
+</html>
