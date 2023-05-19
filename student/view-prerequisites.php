b8c22c177d8d2d67b8c2ca8c1d17bb8167228cf7
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-05-19 17:56:46 +0300

    Completed student/view-prerequisites.php

--- student/view-prerequisites.php
+++ student/view-prerequisites.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * View Course Requisites Page
- * Allows the student user to 
- * view the prerequisites of the
- * selected courses 
+ * View Course Prerequisites Page
+ * Allows the professor user to 
+ * add/delete/update students' grades to the system
+ * 
  * @author Omar Eldanasoury 
  */
 
@@ -14,23 +14,32 @@ if (!isset($_SESSION["activeUser"])) // if the user is not logged in he will be 
     header("location: /course-registration-system/login.php");
 
 require_once("../functions.php");
+if (isset($_POST["get-prereqs"])) {
 
+    // TODO: after user confirms (using JS)
+    // first get data
+    $cid = checkInput($_POST["course-code"]);
 
-$requests = getStudentAppealingRequests($_SESSION["activeUser"][0]);
-$tableBody = "";
-if ($requests == array()) {
-    $tableBody = "<tr><td colspan='4'>You Have No Appealing Requests!</td></tr>";
+    if ($cid == "") { // if the user didn't choose a value for the course or the section
+        $feedbackMsg = "<span class='failed-feedback'>Please select a course!</span>";
+    } else {
+        $prerequisites = getPrerequisites($cid);
+        $tableBody = "";
+
+        // generating the table body based on the data
+        if (empty($prerequisites)) {
+            $tableBody = "<tr><td colspan='3'>The Selected Course Has No Prerequisites!</td></tr>";
+        } else {
+            foreach ($prerequisites as $course) {
+                $tableBody .= "\n<tr>\n<td>" . $course[0] . "</td>\n<td>" . $course[1] . "</td>\n<td>" . $course[2] . "</td>\n</tr>";
+            }
+        }
+    }
 }
 
 
-foreach ($requests as $request) {
-    $code = $request[0];
-    $name = $request[1];
-    $state = $request[2];
-    $grade = $request[3];
 
-    $tableBody .= "<tr>\n<td>$code</td>\n<td>$name</td>\n<td>$state</td>\n<td>$grade</td>\n</tr>\n";
-} // after this, the table will shown as html
+
 ?>
 
 
@@ -41,7 +50,7 @@ foreach ($requests as $request) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>View Appealing Requests</title>
+    <title>View Course Prerequisites</title>
 
     <!-- Adding the css files -->
     <link rel="stylesheet" href="../css/reset.css">
@@ -63,21 +72,51 @@ foreach ($requests as $request) {
 
     <?php require("../header.php");
     require_once("../functions.php");
+    // retrieves the courses that are in the program of the student
+    $courses = getProgramCourses(getStudentProgramId($_SESSION["activeUser"][0]));
     ?>
 
 
     <main class="payment-main" style="background-color: white; background-image: none; text-align: left;">
-        <h1 class="catalogue-header" style="color: #4056A1;">View Appealing Requests</h1>
+        <h1 class="catalogue-header" style="color: #4056A1;">View Course Prerequisites</h1>
+        <form method="post" class="form" style="margin-left: 2.75em;">
+            <div class="attendance-flex catalogue-main">
+                <!-- Course Code and Section Number -->
+                <div class="attendance-inner-flex">
+                    <label for="course-code">Course Code:</label><br><br>
+                    <select class="selecter" onchange="getSections(this.value)" name="course-code" id="course-code">
+                        <option value="">Select a Course</option>
+                        <?php
+                        if ($courses != array())
+                            for ($i = 0; $i < count($courses); $i++)
+                                foreach ($courses[$i] as $id => $code) {
+                                    echo "<option value='" . strval($id) . "'>" . $code . "</option>";
+                                }
+                        ?>
+                    </select>
+                </div>
+
+            </div>
+
+            <input type="submit" class="butn primary-butn sign-butn no-margin-left margin-top small" name="get-prereqs" id="get-prereqs" value="View Course Prerequisites">
+            <br><br>
+            <?php
+            if (isset($feedbackMsg)) {
+                echo $feedbackMsg;
+                unset($feedbackMsg);
+            }
+            ?>
+        </form>
         <!-- The Table of students list -->
         <div class="catalogue-main" style="margin-bottom: 2em;">
             <form method="post" class="form" style="margin-left: 2.75em;  text-align: center;">
-                <table id="displayTable" style="margin: 0;">
+                <table id="displayTable" <?php if (isset($tableBody)) echo "style='visibility: visible; margin: 0;'";
+                                            else echo "style='visibility: hidden;'" ?>>
                     <thead>
                         <tr>
                             <th class="th-color">Course Code</th>
                             <th class="th-color">Course Name</th>
-                            <th class="th-color">Status</th>
-                            <th class="th-color">Grade</th>
+                            <th class="th-color">Credits</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -89,64 +128,10 @@ foreach ($requests as $request) {
                     </tbody>
                 </table>
             </form>
-
         </div>
     </main>
 
     <?php require("../footer.php") ?>
 </body>
-<!-- Script for getting the sections after the user selects the course code :: Using AJAX
-     Author: Omar Eldanasoury
- -->
-<script>
-    /**@function getSection
-     * gets the successive section number to be added
-     * for a particular course id; using AJAX.
-     * 
-     * @author Omar Eldanasoury
-     */
-    function getSections(courseId) {
-        if (courseId == "") {
-            document.getElementById("section-number").innerHTML = "";
-            document.getElementById("section-number").innerHTML += "<option value=''>Select a Course First</option>";
-
-            return;
-        }
-
-        const request = new XMLHttpRequest();
-        request.onload = showSections;
-        request.open("GET", "getProfessorSections.php?cid=" + courseId, false);
-        request.send();
-    }
-
-    /**@function showSection
-     * Shows the section number
-     * retrieved from the database
-     * to the user through html.
-     */
-    function showSections() {
-        clearSectionNumber();
-        if (this.responseText.length == 0) {
-            document.getElementById("section-number").innerHTML += "\n<option value=''>No Sections Available</option>";
-            return
-        }
-        results = this.responseText.split("#");
-        console.log(results);
-        for (let result of results) {
-            idAndNum = result.split("@");
-            if (idAndNum[0] == '')
-                continue;
-            document.getElementById("section-number").innerHTML += "\n<option value='" + idAndNum[0] + "'>" + idAndNum[1] + "</option>";
-        }
-    }
-
-    /**@function clearSectionNumber
-     * clears the html that shows the section number
-     */
-    function clearSectionNumber() {
-        document.getElementById("section-number").innerHTML = "<option value=''>Select a Section</option>";
-
-    }
-</script>
 
 </html>
