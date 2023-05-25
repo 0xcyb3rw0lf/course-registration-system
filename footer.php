cd9d4007a59a3bda879a3ded84988a70a7f083d5
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-05-25 14:52:42 +0300

    Added names and ids to footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
     <p>Copyrights &copy; ITCS489 2022/2023 SEM II; Project</p>
     <p>&copy; Omar Eldanasoury, 202005808</p>
-    <p>&copy; Member Name, ID</p>
-    <p>&copy; Member Name, ID</p>
-    <p>&copy; Member Name, ID</p>
+    <p>&copy; Elyas Raed, 202009262</p>
+    <p>&copy; Mohamed Alammal, 202009144</p>
+    <p>&copy; Abdulmohsen Haidar, 20186070</p>
 </footer>
