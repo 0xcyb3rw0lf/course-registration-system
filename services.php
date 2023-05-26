d6576e58acd9e387af6189b306839bd728322095
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-05-26 16:42:05 +0300

    Added $basePath and updated admin services.php
    Also added add and delete college pages path

--- services.php
+++ services.php
@@ -63,37 +63,42 @@ function getStudentList()
  */
 function getAdminList()
 {
+    $basePath = "/course-registration-system/admin/";
     return array(
         // TODO: Fill the paths of these pages
-        "Add Users" => "/course-registration-system/admin/",
-        "View Users" => '/course-registration-system/admin/',
-        "Update Users" => '/course-registration-system/admin/', // this is the same as managing the profile of users
-        "Delete Users" => "/course-registration-system/admin/",
+        // $basePath . FILE_NAME
+        "Add Users" => $basePath . "add-user.php",
+        "View Users" => $basePath . "",
+        "Update Users" => $basePath . "", // this is the same as managing the profile of users
+        "Delete Users" => $basePath . "",
         "0" => "break", // breaks to separate the options in the front-end
-        "Add Course" => '/course-registration-system/admin/',
-        "View Course" => '/course-registration-system/admin/',
-        "Edit Course" => '/course-registration-system/admin/',
-        "Delete Course" => '/course-registration-system/admin/',
+        "Add Course" => $basePath . "",
+        "View Course" => $basePath . "",
+        "Edit Course" => $basePath . "",
+        "Delete Course" => $basePath . "",
         "1" => "break", // breaks to separate the options in the front-end
-        "Add Section" => '/course-registration-system/admin/add-section.php',
-        "View Section" => '/course-registration-system/admin/view-section.php',
-        "Update Section" => '/course-registration-system/admin/update-section.php',
-        "Delete Section" => '/course-registration-system/admin/delete-section.php',
+        "Add Section" => $basePath . 'add-section.php',
+        "View Section" => $basePath . 'view-section.php',
+        "Update Section" => $basePath . 'update-section.php',
+        "Delete Section" => $basePath . 'delete-section.php',
         "2" => "break", // breaks to separate the options in the front-end
-        "Add Semesters" => '/course-registration-system/admin/',
-        "View Semesters" => '/course-registration-system/admin/',
-        "Edit Semesters" => '/course-registration-system/admin/',
-        "Delete Semesters" => '/course-registration-system/admin/',
+        "Add Semesters" => $basePath . "",
+        "View Semesters" => $basePath . "",
+        "Edit Semesters" => $basePath . "",
+        "Delete Semesters" => $basePath . "",
         "3" => "break", // breaks to separate the options in the front-end
-        "Add Buildings" => '/course-registration-system/admin/add-building.php',
-        "Delete Buildings" => '/course-registration-system/admin/delete-building.php',
-        "Add Room" => '/course-registration-system/admin/add-room.php',
-        "Delete Room" => '/course-registration-system/admin/delete-room.php',
+        "Add Buildings" => $basePath . 'add-building.php',
+        "Delete Buildings" => $basePath . 'delete-building.php',
+        "Add Room" => $basePath . 'add-room.php',
+        "Delete Room" => $basePath . 'delete-room.php',
         "4" => "break", // breaks to separate the options in the front-end
+        "Add College" => $basePath . "add-college.php",
+        "Delete College" => $basePath . "delete-college.php",
         "Add Program" => "",
         "Delete Program" => "",
-        "View Summer Seats" => '/course-registration-system/admin/',
-        "Generate Reports" => '/course-registration-system/admin/',
+        "5" => "break", // breaks to separate the options in the front-end
+        "View Summer Seats" => $basePath . "",
+        "Generate Reports" => $basePath . "",
     );
 }
 
