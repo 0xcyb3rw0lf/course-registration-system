6e98aaf79bc1252d612d9588d58fd83838d9f760
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-05-26 16:17:35 +0300

    Added Completed admin/add-room.php +path to services.php

--- admin/add-room.php
+++ admin/add-room.php
@@ -4,16 +4,22 @@
  * Add Room Page
  * Allows the admin user to 
  * add rooms to the system
+ * 
+ * @author Elyas Raed
+ * @author Omar Eldanasoury
  */
 session_start();
 if (!isset($_SESSION["activeUser"])) // if the user is not logged in he will be redirected to the sign up page
     header("location: /course-registration-system/login.php");
 
+// only admin should access the page
+if (!str_contains($_SESSION["userType"], "admin"))
+    die("You are not allowed to view this page, <a href='/course-registration-system/index.php'>Click Here to Return to Home Page Here!</a>");
+
 if (isset($_POST["add-room"])) {
-    require_once("../functions.php");
+    require_once("../functions2.php");
 
     // Adding the room
-    $roomId = $_POST["room-id"];
     $roomnum = $_POST["room-number"];
     $buildingId = $_POST["bldng"];
     $capacity = $_POST["capacity"];
@@ -26,12 +32,17 @@ if (isset($_POST["add-room"])) {
     ) {
         $feedbackMsg = "<span class='failed-feedback'>Please enter all fields as required!</span>";
     } else {
-            if (addRoom($roomId, $buildingId, $roomnum, $capacity)) {
+        try {
+            if (addRoom($buildingId, $roomnum, $capacity)) {
                 $feedbackMsg = "<span class='success-feedback'>Room is Added Successfully!</span>";
             } else {
-                $feedbackMsg = "<span class='failed-feedback'>Error Adding Room!</span>";
+                $feedbackMsg = "<span class='failed-feedback'>Error Adding Room, Please Try Again Later!</span>";
+            }
+        } catch (Exception $exception) { // send error if room already exists
+            $feedbackMsg = "<span class='failed-feedback'>Room Already Exists, Please Choose a New Room Number!</span>";
+        }
     }
-    }}
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -61,9 +72,9 @@ if (isset($_POST["add-room"])) {
 <body>
 
     <?php require("../header.php");
-    require_once("../functions.php");
+    require_once("../functions2.php");
     // Required varialbes for adding the room
-    $buildings = getBuildings();
+    $buildings = getBuildings(); //get the buildings list from the database
     ?>
 
     <main class="payment-main" style="background-color: white; background-image: none; text-align: left;">
@@ -71,24 +82,16 @@ if (isset($_POST["add-room"])) {
 
         <form method="post" class="form" style="margin-left: 2.75em;">
             <div class="attendance-flex catalogue-main">
-                <!-- Room ID and Room Number -->
+                <!-- Adding Room Number -->
                 <div class="attendance-inner-flex">
-                    <label for="room-id">Room ID:</label><br><br>
-                    <input type="number" min="1" class="selecter" name="room-id" id="room-id">
-                    </select>
-                    <br><br>
-                
-                </div>
-                
-                <div class="attendance-inner-flex" style="margin-left: 2.5em;">
                     <label for="room-number">Room Number:</label><br><br>
                     <input type="number" min="1001" class="selecter" name="room-number" id="room-number">
                     </select>
                     <br><br>
-                
+
                 </div>
 
-                <!-- Building and Capacity -->
+                <!-- Adding Building and Capacity -->
                 <div class="attendance-inner-flex" style="margin-left: 2.5em;">
                     <label for="bldng">Building:</label><br><br>
                     <select onchange="getRooms(this.value)" class="selecter" name="bldng" id="bldng">
@@ -103,12 +106,12 @@ if (isset($_POST["add-room"])) {
                     </select>
                     <br><br><br>
                 </div>
-                
+
             </div>
             <div class="attendance-inner-flex">
-                    <label for="capacity">Capacity:</label><br><br>
-                    <input type="number" min="15" max="9999" class="selecter" name="capacity" id="capacity">
-                </div>
+                <label for="capacity">Capacity:</label><br><br>
+                <input type="number" min="15" max="9999" class="selecter" name="capacity" id="capacity">
+            </div>
 
             <input onclick="return confirm('Are you sure you want to add a room?')" type="submit" class="butn primary-butn sign-butn no-margin-left margin-top small" name="add-room" id="add-room" value="Add a Room">
             <br><br>
@@ -124,4 +127,5 @@ if (isset($_POST["add-room"])) {
 
     <?php require("../footer.php") ?>
 </body>
+
 </html>
--- services.php
+++ services.php
@@ -87,8 +87,8 @@ function getAdminList()
         "3" => "break", // breaks to separate the options in the front-end
         "Add Buildings" => '/course-registration-system/admin/add-building.php',
         "Delete Buildings" => '/course-registration-system/admin/delete-building.php',
-        "Add Room" => '/course-registration-system/admin/',
-        "Delete Room" => '/course-registration-system/admin/',
+        "Add Room" => '/course-registration-system/admin/add-room.php',
+        "Delete Room" => '/course-registration-system/admin/delete-room.php',
         "4" => "break", // breaks to separate the options in the front-end
         "Add Program" => "",
         "Delete Program" => "",
