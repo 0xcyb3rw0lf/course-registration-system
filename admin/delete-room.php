7229ab498b5920c25b9b0eb200283203e9dcf56f
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-05-26 16:31:38 +0300

    Added Completed admin/delete-room.php

--- admin/delete-room.php
+++ admin/delete-room.php
@@ -4,14 +4,22 @@
  * Delete Room Page
  * Allows the admin user to 
  * Delete Rooms from the system
+ * 
+ * @author Omar Eldanasoury
+ * @author Elyas Raed
  */
-
 session_start();
 if (!isset($_SESSION["activeUser"])) // if the user is not logged in he will be redirected to the sign up page
     header("location: /course-registration-system/login.php");
 
+
+// only admin should access the page
+if (!str_contains($_SESSION["userType"], "admin"))
+    die("You are not allowed to view this page, <a href='/course-registration-system/index.php'>Click Here to Return to Home Page Here!</a>");
+
+
 if (isset($_POST["delete-room"])) {
-    require_once("../functions.php");
+    require_once("../functions2.php");
     // first get data
     $rooms = checkInput($_POST["room-number"]);
 
@@ -57,27 +65,36 @@ if (isset($_POST["delete-room"])) {
 <body>
 
     <?php require("../header.php");
-    require_once("../functions.php");
+    require_once("../functions2.php");
     // Required varialbes for deleting the room
-    $rooms = getRooms(); // get the rooms list from the database
+    $buildings = getBuildings(); // get the rooms list from the database
+
     ?>
 
     <main class="payment-main" style="background-color: white; background-image: none; text-align: left;">
         <h1 class="catalogue-header" style="color: #4056A1;">Delete Room</h1>
         <form method="post" class="form" style="margin-left: 2.75em;">
             <div class="attendance-flex catalogue-main">
-                <!-- Course Code and Section Number -->
                 <div class="attendance-inner-flex">
-                    <label for="room-number">Room Number:</label><br><br>
-                    <select class="selecter" name="room-number" id="room-number">
-                        <option value="">Select a Room</option>
+                    <label for="bldng">Building:</label><br><br>
+                    <select onchange="getRooms(this.value)" class="selecter" name="bldng" id="bldng">
+                        <option value="">Select a Building</option>
                         <?php
-                        if ($rooms != array())
-                            for ($i = 0; $i < count($rooms); $i++)
-                                foreach ($rooms[$i] as $id => $code) {
-                                    echo "<option value='" . strval($id) . "'>" . $code . "</option>";
+                        if ($buildings != array())
+                            for ($i = 0; $i < count($buildings); $i++)
+                                foreach ($buildings[$i] as $id => $name) {
+                                    echo "<option value='" . strval($id) . "'>" . $name . "</option>";
                                 }
+
                         ?>
+                    </select>
+                </div>
+                <div class="attendance-inner-flex" style="margin-left: 2.5em;">
+                    <label for="room">Room:</label><br><br>
+                    <select class="selecter" name="room-number" id="room">
+                        <option value="">Select a Room</option>
+                        <!-- The options will be optained from the database using AJAX and PHP -->
+                        <!-- Refer to the script at the end of the page, after <body> -->
                     </select>
                 </div>
 
@@ -96,4 +113,55 @@ if (isset($_POST["delete-room"])) {
 
     <?php require("../footer.php") ?>
 </body>
+<!-- Script for getting the rooms after the user selects the buildings :: Using AJAX
+     Author: Omar Eldanasoury
+ -->
+<script>
+    /**@@function getRooms()
+     * sends user choice to the script to get
+     * rooms of the building
+     * 
+     * @author Omar Eldanasoury
+     */
+    function getRooms(buildingId) {
+        if (buildingId.length == 0) {
+            clearRooms();
+            return;
+        }
+
+        const request = new XMLHttpRequest();
+        request.onload = showRooms;
+        request.open("GET", "getRooms.php?id=" + buildingId);
+        request.send();
+    }
+
+    /**@function showRooms
+     * populated the options inside <select>
+     * after getting the rooms
+     * 
+     * @author Omar Eldanasoury
+     */
+    function showRooms() {
+        clearRooms();
+        results = this.responseText.split("#");
+        for (let result of results) {
+            idAndRoom = result.split("@");
+            if (idAndRoom[0] == '')
+                continue;
+            document.getElementById("room").innerHTML += "\n<option value='" + idAndRoom[0] + "'>" + idAndRoom[1] + "</option>";
+        }
+    }
+
+    /**@function clearRooms
+     * Clears the options to
+     * populate new options when
+     * user's choice changes.
+     * 
+     * @author Omar Eldanasoury
+     */
+    function clearRooms() {
+        document.getElementById("room").innerHTML = "<option value=''>Select a Room</option>"
+    }
+</script>
+
 </html>
