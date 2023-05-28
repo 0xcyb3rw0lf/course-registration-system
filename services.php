be3328629994008282aa938f57f85e42b1ea0b23
Omar Eldanasoury <omardanasoury8@gmail.com>
2023-05-28 18:59:57 +0300

    Added course management paths to services.php

--- services.php
+++ services.php
@@ -72,10 +72,10 @@ function getAdminList()
         "Update Users" => $basePath . "update-user.php", // this is the same as managing the profile of users
         "Delete Users" => $basePath . "delete-user.php",
         "0" => "break", // breaks to separate the options in the front-end
-        "Add Course" => $basePath . "",
-        "View Course" => $basePath . "",
-        "Edit Course" => $basePath . "",
-        "Delete Course" => $basePath . "",
+        "Add Course" => $basePath . "add-course.php",
+        // "View Course" => $basePath . "",
+        "Update Course" => $basePath . "update-course.php",
+        "Delete Course" => $basePath . "delete-course.php",
         "1" => "break", // breaks to separate the options in the front-end
         "Add Section" => $basePath . 'add-section.php',
         "View Section" => $basePath . 'view-section.php',
